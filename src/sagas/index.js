4062610116048b356100876587f4f9a68c365c76
Luke Hodkinson <furious.luke@gmail.com>
2017-12-06 16:46:15 +1100

    Allow multiple DBComponents
    The existing implementation precluded multiple DBComponents in the
    same view. I've changed `takeLatest` to `eachInline` to make sure
    there are no issues with overlapping loads (we can add that in later),
    and I've also removed the call that clears the local cache; this
    should be done manually by the developer somewhere.

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -39,9 +39,12 @@ function* loadModelView( action ) {
         results[name] = null
     }
 
-    // Clear the database prior to loading data. We don't need to
-    // wait for it because we'll be waiting for the next put.
-    yield put( {type: 'MODEL_CLEAR', payload: {schema}} )
+    // TODO: I don't think I need this. Clearing can be nice to keep things
+    // clean, but it precludes the possibility of having different components
+    // use DBComponent.
+    // // Clear the database prior to loading data. We don't need to
+    // // wait for it because we'll be waiting for the next put.
+    // yield put( {type: 'MODEL_CLEAR', payload: {schema}} )
 
     // Merge in the loaded data and wait for it to be finished.
     yield put( {type: 'MODEL_LOAD_JSON', payload: {schema, jsonData}} )
@@ -303,7 +306,7 @@ export function* mutationSerializer( action ) {
  */
 export default function* modelSaga() {
   yield [
-    takeLatest( 'MODEL_LOAD_VIEW', loadModelView ),
+    eachInline( 'MODEL_LOAD_VIEW', loadModelView ),
     eachInline( 'MODEL_SYNC', sync ),
     eachInline(
       [
