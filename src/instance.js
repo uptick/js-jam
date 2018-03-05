5210c10a24495182625a7373f42b95fb3731a422
Luke Hodkinson <furious.luke@gmail.com>
2018-03-05 17:56:27 +1100

    Improve performance by caching instance classes

--- src/instance.js
+++ src/instance.js
@@ -10,13 +10,12 @@ import {
 } from './utils'
 
 /**
- * A convenience class to represent model data.
+ * Base class for instances.
  *
- * Provides getters/setters to assist in modifying model data locally,
- * then saving to the DB later. Also wraps relationships to make
- * finding local DB instances easier.
+ * Holds the common information between on-the-fly instances, created
+ * with the below Instance class, and anonymous model instances.
  */
-export default class Instance {
+export class BaseInstance {
 
   constructor( data, model, db ) {
     if( isEmpty( model ) ) {
@@ -29,95 +28,8 @@ export default class Instance {
     this._db = db
     this._model = model
     this._type = model.type
-    this._initial = data
-    this._prepareAttributes( data )
-    this._prepareRelationships( data )
-    this._values = this._initial
-  }
-
-  /**
-   * Add model attributes to the instance.
-   */
-  _prepareAttributes( data ) {
-    for( const name of this._model.attributes.keys() ) {
-      const attr = this._model.attributes.get( name )
-      if( data[name] !== undefined ) {
-        this._initial = this._initial.set( name, data[name] )
-      }
-      Object.defineProperty( this, name, {
-        get: function() {
-          return this._values.get( name )
-        },
-        set: function( x ) {
-          this._values = this._values.set( name, x )
-        }
-      })
-    }
-  }
-
-  /**
-   * Add model relationships to the instance.
-   */
-  _prepareRelationships( data ) {
-    for( const name of this._model.relationships.keys() ) {
-      const rel = this._model.relationships.get( name )
-      if( rel.get( 'many' ) ) {
-        this._initial = this._initial.set( name, data[name] )
-        Object.defineProperty( this, name, {
-          get: function() {
-            return {
-              all: () => {
-                return this._values.get( name ).map( x =>
-                  this._db.getInstance( x )
-                )
-              },
-              add: x => {
-                this._values = this._values.updateIn( [name], y =>
-                  y.add( this._db.getId( x ) )
-                )
-              },
-              remove: x => {
-                this._values = this._values.updateIn( [name], y =>
-                  y.remove( this._db.getId( x ) )
-                )
-              }
-            }
-          },
-          set: function( x ) {
-            throw ModelError( 'Cannot directly set many-to-many.' )
-          }
-        })
-      }
-      else {
-
-        // Set the initial value if it exists.
-        if( data[name] !== undefined ) {
-          this._initial = this._initial.set( name, data[name] )
-        }
-
-        // The getter for foriegn-keys will return an Instance object
-        // if one exists, and undefined otherwise.
-        Object.defineProperty( this, name, {
-          get: function() {
-
-            // TODO: Use db.get once I've converted it.
-            let obj = this._db.get( this._values.get( name ) )
-            if( obj ) {
-              obj = this._db.schema.toInstance( obj, this._db )
-            }
-            return obj
-          },
-          set: function( x ) {
-            if( x ) {
-              this._values = this._values.set( name, this._db.getId( x ) )
-            }
-            else {
-              this._values = this._values.set( name, x )
-            }
-          }
-        })
-      }
-    }
+    this._initial = new Map()
+    this._values = data
   }
 
   save() {
@@ -140,4 +52,19 @@ export default class Instance {
     return this._model
   }
 
+}
+
+/**
+ * A convenience class to represent model data.
+ *
+ * Provides getters/setters to assist in modifying model data locally,
+ * then saving to the DB later. Also wraps relationships to make
+ * finding local DB instances easier.
+ */
+export default class Instance extends BaseInstance {
+  constructor( ...args ) {
+    super( ...args )
+    this._model.addAttributesToObject( this )
+    this._model.addRelationshipsToObject( this )
+  }
 }
--- src/model.js
+++ src/model.js
@@ -1,12 +1,13 @@
-import { OrderedSet, Set, Record, fromJS } from 'immutable';
+import { OrderedSet, Set, Record, fromJS } from 'immutable'
+import uuid from 'uuid'
 
-import Instance from './instance';
-import { getDiffOp, toArray, ModelError } from './utils';
+import { BaseInstance } from './instance'
+import { getDiffOp, toArray, ModelError } from './utils'
 
 export default class Model {
 
   constructor( type ) {
-    this.type = type;
+    this.type = type
   }
 
   /**
@@ -14,26 +15,27 @@ export default class Model {
    * itself.
    */
   merge( options ) {
-    this.idField = options.idField || 'id';
-    this.attributes = fromJS( options.attributes || {} );
-    this.relationships = fromJS( options.relationships || {} );
-    this.indices = fromJS( options.indices || ['id'] );
-    this.ops = {};
-    this._makeRecord();
+    this.idField = options.idField || 'id'
+    this.attributes = fromJS( options.attributes || {} )
+    this.relationships = fromJS( options.relationships || {} )
+    this.indices = fromJS( options.indices || ['id'] )
+    this.ops = {}
+    this._makeRecord()
+    this._makeInstanceSubclass()
 
     // Build a list of all possible operations, then check if
     // it's been set in options.
-    let operations = ['list', 'create', 'detail', 'update', 'remove', 'options'];
+    let operations = ['list', 'create', 'detail', 'update', 'remove', 'options']
     for( const field of this.iterManyToMany() ) {
-      operations.push( field + 'Add' );
-      operations.push( field + 'Remove' );
+      operations.push( field + 'Add' )
+      operations.push( field + 'Remove' )
     }
     for( const key of operations ) {
       if( options.ops && key in options.ops ) {
         this.ops[key] = (...args) => options.ops[key]( ...args ).then( data => {
-          console.debug( `Model: ${key}: `, data );
-          return data;
-        });
+          console.debug( `Model: ${key}: `, data )
+          return data
+        })
       }
     }
   }
@@ -44,96 +46,187 @@ export default class Model {
       [this.idField]: undefined
     }
     this.attributes.forEach( (attr, name) => {
-      data[name] = attr.get( 'default' );
+      data[name] = attr.get( 'default' )
     })
     this.relationships.forEach( (rel, name) => {
-      data[name] = rel.get( 'many' ) ? new OrderedSet() : undefined;
+      data[name] = rel.get( 'many' ) ? new OrderedSet() : undefined
     })
-    this._record = Record( data );
+    this._record = Record( data )
+  }
+
+  /**
+   * Add attribute getters/setters to object.
+   */
+  addAttributesToObject( obj ) {
+    for( const name of this.attributes.keys() ) {
+      const attr = this.attributes.get( name )
+      Object.defineProperty( obj, name, {
+        get: function() {
+          return this._values.get( name )
+        },
+        set: function( x ) {
+          this._values = this._values.set( name, x )
+        }
+      })
+    }
+  }
+
+  /**
+   * Add relationship getters/setters to object.
+   */
+  addRelationshipsToObject( obj ) {
+    for( const name of this.relationships.keys() ) {
+      const rel = this.relationships.get( name )
+      if( rel.get( 'many' ) ) {
+        Object.defineProperty( obj, name, {
+          get: function() {
+            return {
+              all: () => {
+                return this._values.get( name ).map( x =>
+                  this._db.getInstance( x )
+                )
+              },
+              add: x => {
+                this._values = this._values.updateIn( [name], y =>
+                  y.add( this._db.getId( x ) )
+                )
+              },
+              remove: x => {
+                this._values = this._values.updateIn( [name], y =>
+                  y.remove( this._db.getId( x ) )
+                )
+              }
+            }
+          },
+          set: function( x ) {
+            throw ModelError( 'Cannot directly set many-to-many.' )
+          }
+        })
+      }
+      else {
+
+        // The getter for foriegn-keys will return an Instance object
+        // if one exists, and undefined otherwise.
+        Object.defineProperty( obj, name, {
+          get: function() {
+
+            // TODO: Use db.get once I've converted it.
+            let obj = this._db.get( this._values.get( name ) )
+            if( obj ) {
+              obj = this._db.schema.toInstance( obj, this._db )
+            }
+            return obj
+          },
+          set: function( x ) {
+            if( x ) {
+              this._values = this._values.set( name, this._db.getId( x ) )
+            }
+            else {
+              this._values = this._values.set( name, x )
+            }
+          }
+        })
+      }
+    }
+  }
+
+  /**
+   * Make a model specific instance class.
+   *
+   * Creating Instance classes tends to involve a lot of iterating
+   * over fields and adding methods to classes. Here we define an
+   * anonymous class prebuilt with the setters and getters, so we
+   * don't need to do it each time we send back an instance.
+   */
+  _makeInstanceSubclass() {
+    this.Instance = class extends BaseInstance {
+    }
+    this.addAttributesToObject( this.Instance.prototype )
+    this.addRelationshipsToObject( this.Instance.prototype )
   }
 
   addReverseRelationship( field, relation ) {
     if( !this.relationships.has( field ) ) {
-      this.relationships = this.relationships.set( field, relation );
-      this._makeRecord();
+      this.relationships = this.relationships.set( field, relation )
+      this._makeRecord()
     }
     // TODO: Check that the fields are compatible.
   }
 
   update( obj, values ) {
     Object.keys( values ).forEach( field => {
-      obj = obj.set( field, values[field] );
-    });
-    return obj;
+      obj = obj.set( field, values[field] )
+    })
+    return obj
   }
 
   toInstance( objData, db ) {
-    return new Instance( objData, this, db );
+    return new this.Instance( objData, this, db )
   }
 
   toObject( objData, db ) {
-    let obj = new this._record( objData || {} );
+    let obj = new this._record( objData || {} )
     this.relationships.forEach( (rel, name) => {
       if( rel.get( 'many' ) ) {
-        let val = obj.get( name );
+        let val = obj.get( name )
         if( !OrderedSet.isOrderedSet( val ) && !Set.isSet( val ) )
-          val = toArray( val );
+          val = toArray( val )
         obj = obj.set( name, new OrderedSet(
           val.map( x => db.makeId( x ) )
-        ));
+        ))
       }
       else if( obj[name] ) {
-        obj = obj.set( name, db.makeId( obj[name] ) );
+        obj = obj.set( name, db.makeId( obj[name] ) )
       }
-    });
-    return obj;
+    })
+    return obj
   }
 
   *iterFields(opts) {
-    yield '_type';
-    yield 'id';
+    yield '_type'
+    yield 'id'
     for( const x of this.attributes.keys() )
-      yield x;
+      yield x
     for( const x of this.iterRelationships(opts) )
-      yield x;
+      yield x
   }
 
   *iterRelationships(opts) {
     for( const x of this.iterForeignKeys(opts) )
-      yield x;
+      yield x
     for( const x of this.iterManyToMany(opts) )
-      yield x;
+      yield x
   }
 
   *iterManyToMany( opts ) {
-    const {includeReverse=false} = opts || {};
+    const {includeReverse=false} = opts || {}
     for( const field of this.relationships.keys() ) {
       if( (!includeReverse && this.relationships.getIn( [field, 'reverse'] ))
           || !this.relationships.getIn( [field, 'many'] ) )
       {
-        continue;
+        continue
       }
-      yield field;
+      yield field
     }
   }
 
   *iterForeignKeys( opts ) {
-    const { includeReverse = false } = opts || {};
+    const { includeReverse = false } = opts || {}
     for( const field of this.relationships.keys() ) {
       if( (!includeReverse && this.relationships.getIn( [field, 'reverse'] ))
           || this.relationships.getIn( [field, 'many'] ) )
       {
-        continue;
+        continue
       }
-      yield field;
+      yield field
     }
   }
 
   fieldIsForeignKey( field ) {
-    const info = this.relationships.get( field );
+    const info = this.relationships.get( field )
     if( info === undefined )
-      return false;
-    return !info.get( 'many' );
+      return false
+    return !info.get( 'many' )
   }
 
   fieldIsManyToMany( field ) {
@@ -162,15 +255,15 @@ export default class Model {
   }
 
   diff( fromObject, toObject ) {
-    let diff = {};
+    let diff = {}
 
     // Check for creation.
     if( fromObject === undefined ) {
       if( toObject === undefined )
-        return;
+        return
       for( const field of this.iterFields() ) {
         if( toObject[field] !== undefined )
-          diff[field] = [undefined, toObject[field]];
+          diff[field] = [undefined, toObject[field]]
       }
     }
 
@@ -178,41 +271,41 @@ export default class Model {
     else if( toObject === undefined ) {
       for( const field of this.iterFields() ) {
         if( fromObject[field] !== undefined )
-          diff[field] = [fromObject[field], undefined];
+          diff[field] = [fromObject[field], undefined]
       }
     }
 
     // Use field differences.
     else {
-      let size = 0;
+      let size = 0
       for( const field of this.iterFields() ) {
-        diff[field] = [fromObject[field], toObject[field]];
+        diff[field] = [fromObject[field], toObject[field]]
         if( field == '_type' || field == 'id' )
-          continue;
+          continue
         if( diff[field][0] == diff[field][1] )
-          delete diff[field];
+          delete diff[field]
         else {
-          const relInfo = this.relationships.get( field );
+          const relInfo = this.relationships.get( field )
           if( relInfo ) {
             if( diff[field][0] && diff[field][0].equals( diff[field][1] ) )
-              delete diff[field];
+              delete diff[field]
             else if( relInfo && relInfo.get( 'many' ) ) {
-              size += 1;
-              diff[field][0] = fromObject[field].subtract( toObject[field] );
-              diff[field][1] = toObject[field].subtract( fromObject[field] );
+              size += 1
+              diff[field][0] = fromObject[field].subtract( toObject[field] )
+              diff[field][1] = toObject[field].subtract( fromObject[field] )
             }
             else
-              size += 1;
+              size += 1
           }
           else
-            size += 1;
+            size += 1
         }
       }
       if( !size )
-        return;
+        return
     }
 
-    return diff;
+    return diff
   }
 
   diffToJsonApi( diff ) {
@@ -221,24 +314,24 @@ export default class Model {
       id: diff.id[1],
       attributes: {},
       relationships: {}
-    };
-    const op = getDiffOp( diff );
+    }
+    const op = getDiffOp( diff )
     if( op == 'remove' ) {
-      data.type = diff._type[0];
-      data.id = diff.id[0];
+      data.type = diff._type[0]
+      data.id = diff.id[0]
     }
     for( const field of this.attributes.keys() ) {
       if( field in diff )
-        data.attributes[field] = diff[field][1];
+        data.attributes[field] = diff[field][1]
     }
     for( const field of this.iterForeignKeys() ) {
       if( field in diff ) {
-        const x = diff[field][1];
+        const x = diff[field][1]
         data.relationships[field] = {
           data: x ? {type: x._type, id: x.id} : null
-        };
+        }
       }
     }
-    return {data};
+    return {data}
   }
 }
